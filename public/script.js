ee037afbe646aefe01e4e592a60b0f2a2308a765
Isaac Kuhlmann <isaac.kuhlmann@ku.edu>
2020-09-09 18:39:44 -0500

    Merging

--- public/script.js
+++ public/script.js
@@ -1,10 +1,9 @@
-import Space from "./space.js"
-
 // Example player board representation
 // Isaac: I marked the indices with x and y to make it clearer
 // X's go from left to right
 // Y's go from top to bottom 
-const player1Board = () => {
+
+newBoard = () => {
     let board = []
     for(let y=1; y<10; y++){
         let row = []
@@ -18,18 +17,18 @@ const player1Board = () => {
     return board
 }
 
+const player1Board = newBoard();
+const player2Board = newBoard();
+
 // Takes a Player's board representation and maps the values
 // onto the HTML Table
-const mapToGrid = (board) => {
-    let gameGrid1 = document.querySelector("#game-grid-1");
-    let gameGrid2 = document.querySelector("#game-grid-2");
-    for (let i=0; i<9; i++){
-        for(let j=0; j<9; j++){
-            console.log(board[i][j].coordinate)
-            gameGrid1.children[0].children[i].children[j].innerText = board[i][j].state;
-            gameGrid2.children[0].children[i].children[j].innerText = board[i][j].state;
-        }
-    }
+const mapToGrid = (board, boardId) => {
+    let gameGrid = document.querySelector(boardId);
+    board.forEach((row, index1) => {
+        row.forEach((cell, index2) => {
+            gameGrid.children[0].children[index1].children[index2].innerHTML = cell;
+        })
+    })
 }
 
 const toggleColor = (cell, color) => {
@@ -45,9 +44,9 @@ const toggleColor = (cell, color) => {
 
 // Allows for the execution of a callback function
 // on every single grid within the Game Table
-const execOnGrid = (fn) => {
+const execOnGrid = (boardId, fn) => {
     console.log(`Executing ${fn.name}`)
-    let gameGrid = document.querySelector("#game-grid-1");
+    let gameGrid = document.querySelector(boardId);
     for (let row of gameGrid.children[0].children) {
         for (let cell of row.children) {
             fn(cell);
@@ -58,8 +57,85 @@ const execOnGrid = (fn) => {
 // Starts the game
 const startGame = () => {
     console.log("Starting Game");
-    let board = player1Board();
-    mapToGrid(board);
+    mapToGrid(player1Board, "#game-grid-1");
+    mapToGrid(player2Board, "#game-grid-2");
 }
 
-document.querySelector("#start").addEventListener("click", startGame);
+/*
+* Method: gameOver
+* Pre: None
+* Params: winnerName: "The winners board"
+* Post: Game is reset and winner is announced
+*/
+const gameOver = (winnerName) => {
+    clearBoard("#game-grid-1");
+    clearBoard("#game-grid-2");
+    console.log(`Game Won by: ${winnerName}`);
+    alert(`Game Won by: ${winnerName}`)
+}
+
+/*
+* Method: clearBoard
+* Pre: None
+* Params: boardName: "Name of the board being cleared"
+* Post: The game board cell values, styles, and onclicks are removed
+*/
+const clearBoard = (boardName) => {
+    execOnGrid(boardName, (cell) => {
+        cell.innerHTML = "";
+        cell.style = "";
+        cell.onclick = "";
+    })
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    /*Fire Command
+    
+    - Waiting for what the states of each board is (Whoever is working on it)
+    Adding even listeners to every table and cell when attacking 
+    Need name of gameboard2 to contiue then change turns 
+    
+    const fireturn =(playerboard) => 
+    {}
+    */
+
+    let gameboard1 = document.getElementById("game-grid-1");
+    for (let i = 0; i < gameboard1.rows.length; i++) {
+        for (let j = 0; j < gameboard1.rows[i].cells.length; j++) {
+            gameboard1.rows[i].cells[j].addEventListener("click", (cell) => {
+                console.log("clicked on this cell");
+                if (gameboard1.rows[i].cells[j].innerHTML == "1") //Will be S if its a ship using 1 to test 
+                {
+                    gameboard1.rows[i].cells[j].innerHTML = "HIT"; //WIll be changed to H if its a hit
+                }
+                else {
+                    gameboard1.rows[i].cells[j].innerHTML = "MISS"; //WIll be changed to H if its a hit
+                }
+            });
+
+        }
+
+    }
+
+    let gameboard2 = document.getElementById("game-grid-2");
+    for (let i = 0; i < gameboard2.rows.length; i++) {
+        for (let j = 0; j < gameboard2.rows[i].cells.length; j++) {
+            gameboard2.rows[i].cells[j].addEventListener("click", (cell) => {
+                console.log("clicked on this cell");
+                if (gameboard2.rows[i].cells[j].innerHTML == "1") //Will be S if its a ship using 1 to test 
+                {
+                    gameboard2.rows[i].cells[j].innerHTML = "HIT"; //WIll be changed to H if its a hit
+                }
+                else {
+                    gameboard2.rows[i].cells[j].innerHTML = "MISS"; //WIll be changed to H if its a hit
+                }
+            });
+
+        }
+
+    }
+
+})
